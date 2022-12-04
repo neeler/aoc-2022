8d7ab7c99f2ebadce77d8cc482839b0d625883f0
Neel Rao <neelorao@gmail.com>
2022-12-03 21:22:39 -0800

    feat: puzzle 4

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { puzzle1 } from '~/puzzle1';
 import { puzzle2 } from '~/puzzle2';
 import { puzzle3 } from '~/puzzle3';
+import { puzzle4 } from '~/puzzle4';
 
 puzzle1.run();
 puzzle2.run();
 puzzle3.run();
+puzzle4.run();
---
+++ src/puzzle4.ts
@@ -0,0 +1,68 @@
+import { Puzzle } from '~/Puzzle';
+
+interface AssignmentAnalysis {
+    min: number;
+    max: number;
+    sections: number[];
+    sectionMap: Record<string, boolean>;
+}
+
+interface PairAnalysis {
+    elf1: AssignmentAnalysis;
+    elf2: AssignmentAnalysis;
+}
+
+function analyzeAssigment(assignment: string): AssignmentAnalysis {
+    const [min, max] = assignment
+        .split('-')
+        .map((section) => parseInt(section, 10));
+    const sections = Array.from({ length: max - min + 1 }, (v, i) => i + min);
+    return {
+        min,
+        max,
+        sections,
+        sectionMap: sections.reduce((map, section) => {
+            map[section] = true;
+            return map;
+        }, {} as Record<string, boolean>),
+    };
+}
+
+export const puzzle4 = new Puzzle({
+    day: 4,
+    processFile: (fileData): PairAnalysis[] =>
+        fileData
+            .trim()
+            .split(/\s/)
+            .map((pair) => {
+                const [elf1, elf2] = pair.split(',') as [string, string];
+                return {
+                    elf1: analyzeAssigment(elf1),
+                    elf2: analyzeAssigment(elf2),
+                };
+            }),
+    part1: (pairs) => {
+        const containedPairs = pairs.filter(
+            (pair) =>
+                pair.elf1.sections.every(
+                    (section) => pair.elf2.sectionMap[section]
+                ) ||
+                pair.elf2.sections.every(
+                    (section) => pair.elf1.sectionMap[section]
+                )
+        );
+        return containedPairs.length;
+    },
+    part2: (pairs) => {
+        const overlappingPairs = pairs.filter(
+            (pair) =>
+                pair.elf1.sections.some(
+                    (section) => pair.elf2.sectionMap[section]
+                ) ||
+                pair.elf2.sections.some(
+                    (section) => pair.elf1.sectionMap[section]
+                )
+        );
+        return overlappingPairs.length;
+    },
+});
